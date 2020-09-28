a806e3a3e327e0e78f023b85e74029e4894a20d6
vsensu <vsensu@foxmail.com>
2020-09-28 18:44:27 +0800

    Some deps may need build from source

--- setup.bat
+++ setup.bat
@@ -1,2 +1,2 @@
-conan install . -s build_type=Debug -s arch=x86 -if build_debug
-conan install . -s build_type=Release -s arch=x86 -if build_release
+conan install . -s build_type=Debug -s arch=x86 -if build_debug --build=enet --build=glfw --build=imgui
+conan install . -s build_type=Release -s arch=x86 -if build_release --build=enet --build=glfw --build=imgui
